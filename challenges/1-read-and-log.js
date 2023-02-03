26acffbeeac90dafca1b3394282b92d16652424a
infoomics <sbx_94@hotmail.com>
2023-02-03 19:27:45 +0000

    reorganised

--- challenges/1-read-and-log.js
+++ challenges/1-read-and-log.js
@@ -1,6 +1,7 @@
 const fs = require('fs/promises');
 
-fs.readFile(`${__dirname}/secret-message.txt`, 'utf-8')
+fs
+.readFile(`${__dirname}/secret-message.txt`, 'utf-8')
 .then( (data) => {
     console.log(data);
 } )
