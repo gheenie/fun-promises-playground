ae9dbfddbb80d45cad06ee0ea856ed739d644ea9
infoomics <sbx_94@hotmail.com>
2023-02-03 17:14:16 +0000

    added catch block

--- challenges/1-read-and-log.js
+++ challenges/1-read-and-log.js
@@ -3,4 +3,7 @@ const fs = require('fs/promises');
 fs.readFile(`${__dirname}/secret-message.txt`, 'utf-8')
 .then( (data) => {
     console.log(data);
+} )
+.catch( (err) => {
+    console.log(err);
 } );
